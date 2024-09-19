74c7a0201fa26044b2426ea175ee4cab992ddc18
Coding Wizard <CodingWizard@hacknexus.xyz>
2024-09-19 18:20:55 +0530

    refactor: modified start handler to welcome  in proper parsing mode

--- bot/handlers/start.py
+++ bot/handlers/start.py
@@ -1,7 +1,11 @@
+from aiogram.enums import ParseMode
 from aiogram.filters import Command
 from aiogram.types import Message
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 from bot.dispatcher import dp
+
+start_kb = InlineKeyboardBuilder()
 
 
 @dp.message(Command("start"))
@@ -9,6 +13,10 @@ async def command_start_handler(message: Message):
     """
     This handler receives messages with `/start` command
     """
+    start_message = f"Welcome, <b>{message.from_user.full_name}</b>!"
     await message.answer(
-        f"Hello {message.from_user.full_name}!, Hi, welcome to ollama!"
+        start_message,
+        parse_mode=ParseMode.HTML,
+        reply_markup=start_kb.as_markup(),
+        disable_web_page_preview=True,
     )
